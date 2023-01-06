8c43ce3862f441d95e9397ad05f4604960689482
Kenneth-Shifa <kennethshifa@gmail.com>
2023-01-06 18:55:23 +0600

    Teacher records

--- Jan06.py
+++ Jan06.py
@@ -1,1 +1,17 @@
-print('hello')
+#show all teachers
+lines = []
+with open("temp.txt") as temp:
+    for line in temp:
+        lines.append(line)
+for line in lines:
+    print(line)
+#show all english teachers
+for line in lines:
+    sub=line.split('-')[1]
+    if(sub=='English'):
+        print(line)
+
+
+#show all teachers whose salary is more than 2000
+
+#show all teachers who Math english and salary is more than 2000
